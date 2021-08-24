27641d6b35e4a3cb5632b81749f2f6f28804efe3
JustinzDev <fullyz1532@gmail.com>
2021-08-24 19:38:39 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shopee</title>
+    <link href="assets/css/main.css?v=<?=time();?>" rel="stylesheet">
+    <link href="assets/fontawesome/css/all.min.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <header>    
+        <div class="navbartop">
+            <div class="navbar-warpper">
+                <div class="navbar-warpperleft">
+                    <a class="anormal" href="#">Seller Centre</a>
+                    <a class="anormal" href="#">ขายสินค้ากับช้อปปี้</a>
+                    <a class="anormal"href="#">ดาวน์โหลด</a>
+                    <a class="aspec" href="#">ติดต่อเราบน</a>
+                    <a class="iconapp" href="#"><i class="fab fa-facebook"></i></a>
+                    <a class="iconapp" href="#"><i class="fab fa-instagram"></i></a>
+                    <a class="iconapp" href="#"><i class="fab fa-line"></i></a>
+                </div>
+
+                <div class="navbar-warpperright">
+                
+                    <a class="iconapp" href="#"><i class="far fa-bell"></i></a>
+                    <a class="aspec right" href="#">การแจ้งเตือน</a>
+                    <a class="iconapp" href="#"><i class="far fa-question-circle"></i></a>
+                    <a class="aspec right" href="#">ช่วยเหลือ</a>
+                    <a class="aspec right" href="#">สมัครใหม่</a>
+                    <a class="alogin right" href="#">เข้าสู่ระบบ</a>
+                </div>
+            </div>
+            <div class="navbar-warpperdown">
+                <img src="assets/img/logo.png">
+                <input type="text" placeholder=" ค้นหาสินค้าและร้านค้า" name="search">
+                <button type="submit"><i class="fa fa-search"></i></button>
+                <i class="fas fa-shopping-cart cart"></i>
+            </div>
+            
+            <div class="navbar-warpperdowntoo">
+                <a href="#">ผักอบกรอบ</a>
+                <a href="#">ชุดนอน</a>
+                <a href="#">รองเท้าแตะผู้หญิง</a>
+                <a href="#">แมส</a>
+                <a href="#">กางเกงขาสั้น</a>
+                <a href="#">เสื้อยืด</a>
+                <a href="#">เสื้อครอป</a>
+                <a href="#">ชั้นวางของ</a>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
