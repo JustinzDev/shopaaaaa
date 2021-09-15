30e244b69cd06d460378d3c1b0ca2cb5ebc3f512
A12TNiM <sittichoke2222@gmail.com>
2021-09-15 20:37:31 +0700

    edit login

--- login.php
+++ login.php
@@ -12,39 +12,130 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 <body>
-<header>    
-    <div class="navbartop">
-        <div class="logo">
-            <img src="./assets/img/logologin.png" alt="">
-        </div>
-        <h4>เข้าสู่ระบบ</h4>
-        
-    </div>
-    <a href="#" class="help">ต้องการความช่วยเหลือ?</a>
-</header>
-<div id="main">
-    <div clsss="contain">
-        <div class="mainbody">
-            <div class="leftbody">
-                <img src="./assets/img/logo.png"><br><br>
-                <h2>
-                    แหล่งช้อปปิ้งออนไลน์ที่ใหญ่ที่สุดในจักรวาล<br>
-                    กาแลกซี่พร้อมขายอินฟินิตี้สโตน
-                </h2>
+    <header>    
+        <div class="navbartop">
+            <div class="logo">
+                <img src="./assets/img/logologin.png" alt="">
             </div>
-            <div class="rightbody">
-                <form action="#" method="POST">
-                    <h5>เข้าสู่ระบบ</h5>
-                    <input type="text" name="Uname" placeholder="หมายเลขโทรศัพท์ / Email / ชื่อผู้ใช้"><br>
-                    <input type="password" name="pw1" placeholder="รหัสผ่าน"> <br>
-                    <button type="submit">เข้าสู่ระบบ</button><br>
-                    <a href="#" style="float: right; font-size: 12px; margin-top: 3px;">ลืมรหัสผ่าน</a><br>
-
-                    <span style="font-size: 14px; color: grey">เพิ่งเคยเข้ามาใน Shopee ใช่หรือไม่</span><a href="#" style="font-size: 14px;  color: #ee4d2d;"> สมัครใหม่</a>
-                </form>
+            <h4>เข้าสู่ระบบ</h4>
+            
+        </div>
+        <a href="#" class="help">ต้องการความช่วยเหลือ?</a>
+    </header>
+    <div id="main">
+        <div clsss="contain">
+            <div class="mainbody">
+                <div class="leftbody">
+                    <img src="./assets/img/shopatanglogo.png"><br><br>
+                    <h2>
+                        แหล่งช้อปปิ้งออนไลน์ที่ใหญ่ที่สุดในจักรวาล<br>
+                        กาแลกซี่พร้อมขายอินฟินิตี้สโตน
+                    </h2>
+                </div>
+                <div class="rightbody">
+                    <form action="#" method="POST">
+                        <h5>เข้าสู่ระบบ</h5>
+                        <input type="text" name="Uname" placeholder="หมายเลขโทรศัพท์ / Email / ชื่อผู้ใช้"><br>
+                        <input type="password" name="pw1" placeholder="รหัสผ่าน"> <br>
+                        <button type="submit">เข้าสู่ระบบ</button><br>
+                        <a href="#" style="float: right; font-size: 12px; margin-top: 3px;">ลืมรหัสผ่าน</a><br>
+                        <div class="spana">
+                            <span >เพิ่งเคยเข้ามาใน Shopee ใช่หรือไม่</span><a href="#"> สมัครใหม่</a>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
-</div>
+    <footer>
+        <div class="footer1">
+            <div class="row1">
+                <h6>ศูนย์ช่วยเหลือ</h6>
+                <a href="#">Help Center</a>
+                <a href="#">สั่งซื้อสินค้าอย่างไร</a>
+                <a href="#">เริ่มขายสินค้าอย่างไร</a>
+                <a href="#">ช่องทางการชำระเงินใน Shopa</a>
+                <a href="#">Shopa Coins</a>
+                <a href="#">การจัดส่งสินค้า</a>
+                <a href="#">การคืนเงินและคืนสินค้า</a>
+                <a href="#">การันตีโดย Shopa คืออะไร?</a>
+                <a href="#">ติดต่อ Shopa</a>
+            </div>
+            <div class="row2">
+                <h6>เกี่ยวกับ SHOPA</h6>
+                <a href="#">โปรแกรม Affiliate</a>
+                <a href="#">ร่วมงานกับเรา</a>
+                <a href="#">นโยบายของ Shopa</a>
+                <a href="#">ช่องทางการชำระเงินใน Shopa</a>
+                <a href="#">นโยบายความเป็นส่วนตัว</a>
+                <a href="#">Shopa Blog</a>
+                <a href="#">Shopa Mall</a>
+                <a href="#">Seller Centre</a>
+                <a href="#">Flash Deals</a>
+                <a href="#">ผู้ติดต่อออนไลน์</a>
+            </div>
+            <div class="row3">
+                <h6>วิธีการชำระเงิน</h6>
+                <div class="howpay">
+                    <img src="./assets/img/howpay1.png" class="hp1">
+                    <img src="./assets/img/howpay2.png" class="hp2">
+                    <img src="./assets/img/howpay3.png" class="hp3"><br>
+                    <img src="./assets/img/howpay4.png" class="hp4">
+                    <img src="./assets/img/howpay5.png" class="hp5">
+                    <img src="./assets/img/howpay6.png" class="hp6"><br>
+                    <img src="./assets/img/howpay7.png" class="hp7">
+                    <img src="./assets/img/howpay8.png" class="hp8">
+                    <img src="./assets/img/howpay9.png" class="hp9"><br>
+                    <img src="./assets/img/howpay10.png" class="hp10">
+                    <img src="./assets/img/howpay11.png" class="hp11"><br>
+                </div><br>
+                <h6>บริการจัดส่ง</h6>
+                <div class="delivery">
+                    <img src="./assets/img/delivery1.png" class="der1">
+                    <img src="./assets/img/delivery2.png" class="der2">
+                    <img src="./assets/img/delivery3.png" class="der3"><br>
+                    <img src="./assets/img/delivery4.png" class="der4">
+                    <img src="./assets/img/delivery5.png" class="der5">
+                    <img src="./assets/img/delivery6.png" class="der6"><br>
+                    <img src="./assets/img/delivery7.png" class="der7">
+                    <img src="./assets/img/delivery8.png" class="der8">
+                    <img src="./assets/img/delivery9.png" class="der9"><br>
+                </div>
+                
+            </div>
+            <div class="row4">
+                <h6>ติดตามเรา</h6>
+                <a href="#">
+                    <img src="./assets/img/contact1.png"><h5>facebook</h5>
+                </a>
+                <a href="#">
+                    <img src="./assets/img/contact2.png"><h5>Instagram</h5>
+                </a>
+                <a href="#">
+                    <img src="./assets/img/contact3.png"><h5>Line</h5>
+                </a>
+                <a href="#">
+                    <img src="./assets/img/contact4.png"><h5>Linkedln</h5>
+                </a>
+            </div>
+            <div class="row5">
+                <h6>ดาวน์โหลดแอปพลิเคชั่น</h6>
+                <div class="download">
+                    <a href="#">
+                        <img src="./assets/img/download1.png" style="width: 80px;">
+                    </a>
+                    <a href="#">
+                        <img src="./assets/img/download2.png" style="width: 80px;">
+                    </a>
+                    <a href="#">
+                        <img src="./assets/img/download3.png" style="width: 80px;">
+                    </a>
+                </div> 
+            </div>
+        </div>
+        <div class="footer2">
+            © 2021 Shopa. All Rights Reserved
+        </div>
+    </footer>
 </body>
 </html>
