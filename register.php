ec821cf6074975c5508fbda27066d77543d75f73
A12TNiM <sittichoke2222@gmail.com>
2021-09-21 12:46:42 +0700

    แก้title

--- register.php
+++ register.php
@@ -14,7 +14,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>สมัครสมาชิกช็อปปี้ออนไลน์ที่นี่</title>
+    <title>สมัครสมาชิกช็อปป๊ะออนไลน์ที่นี่</title>
     <link href="assets/css/login.css?v=<?=time();?>" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
