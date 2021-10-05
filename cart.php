f2f3215805336fe3ae23a0cd24bff8704493835d
JustinzDev <fullyz1532@gmail.com>
2021-10-05 11:31:35 +0700

    แก้บัค

---
+++ cart.php
@@ -0,0 +1,39 @@
+<?php
+    session_start();
+    include('mysql_connection/my_connection.php');
+    error_reporting(0);
+    include('api/setlink.php');
+
+    if($_SESSION['Uall_id'] == ""){
+		echo "<script>window.location='".$vps."';</script>";
+        exit();
+    }
+
+    $sql = "SELECT *  FROM (products INNER JOIN accounts ON products.acc_id = accounts.acc_id) WHERE product_id = '".$_GET['itemid']."'";
+    $query = mysqli_query($conn, $sql);
+    $result = mysqli_fetch_array($query, MYSQLI_ASSOC);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shopa</title>
+    <link href="<?php echo $vps;?>assets/css/main.css?v=<?=time();?>" rel="stylesheet">
+    <link href="<?php echo $vps;?>assets/fontawesome/css/all.min.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert-dev.js"></script>
+</head>
+<body style="background-color: rgba(156, 156, 156, 0.1);">
+    <?php include('navbar.php');?>
+    <div clsss="contain">
+        
+    </div>
+</body>
+</html>
--- user/myshop/navbarleftshop.php
+++ user/myshop/navbarleftshop.php
@@ -23,8 +23,8 @@
         });
     </script>
     <title>Shopa</title>
-    <link href="<?php echo $mylocalhost;?>assets/css/shop.css?v=<?=time();?>" rel="stylesheet">
-    <link href="<?php echo $mylocalhost;?>assets/fontawesome/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo $vps;?>assets/css/shop.css?v=<?=time();?>" rel="stylesheet">
+    <link href="<?php echo $vps;?>assets/fontawesome/css/all.min.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
@@ -35,35 +35,35 @@
 <body>
     <div class="navbarshow-hide"><button type="button" onclick="clicknavbar()">Show/Hide Navbar</button></div>
     <ul id="nav">
-        <li><a href="#" class="sub" tabindex="1"><img src="<?php echo $mylocalhost;?>/assets/img/side.png" />การจัดส่ง</a><img alt="" />
+        <li><a href="#" class="sub" tabindex="1"><img src="<?php echo $vps;?>/assets/img/side.png" />การจัดส่ง</a><img alt="" />
             <ul>
                 <li><a href="#" data-target="manageitem">การจัดส่งของฉัน</a></li>
                 <li><a href="#">จัดส่งแบบชุด</a></li>
                 <li><a href="#">ตั้งค่าการจัดส่ง</a></li>
             </ul>
         </li>
-        <li><a href="#" class="sub" tabindex="1"><img src="<?php echo $mylocalhost;?>/assets/img/side2.png" />คำสั่งซื้อ</a><img alt="" />
+        <li><a href="#" class="sub" tabindex="1"><img src="<?php echo $vps;?>/assets/img/side2.png" />คำสั่งซื้อ</a><img alt="" />
             <ul>
                 <li><a href="#">คำสั่งซื้อของฉัน</a></li>
                 <li><a href="#">การยกเลิก</a></li>
                 <li><a href="#">การคืนเงิน/คืนสินค้า</a></li>
             </ul>
         </li>
-        <li><a href="#" class="sub" tabindex="1"><img src="<?php echo $mylocalhost;?>/assets/img/side3.png" />สินค้า</a><img  alt="" />
+        <li><a href="#" class="sub" tabindex="1"><img src="<?php echo $vps;?>/assets/img/side3.png" />สินค้า</a><img  alt="" />
             <ul>
                 <li><a href="#" data-target="myitemshop">สินค้าของฉัน</a></li>
                 <li><a href="#" data-target="additemshop">เพิ่มสินค้าใหม่</a></li>
                 <li><a href="#">สินค้าที่ถูกระงับ</a></li>
             </ul>
         </li>
-        <li><a href="#" class="sub" tabindex="1"><img src="<?php echo $mylocalhost;?>/assets/img/side4.png" />Marketing Centre</a><img  alt="" />
+        <li><a href="#" class="sub" tabindex="1"><img src="<?php echo $vps;?>/assets/img/side4.png" />Marketing Centre</a><img  alt="" />
             <ul>
                 <li><a href="#">Marketing Centre</a></li>
                 <li><a href="#">Shopa Ads</a></li>
                 <li><a href="#">โค้ดส่วนลด</a></li>
             </ul>
         </li>
-        <li><a href="#" class="sub" tabindex="1"><img src="<?php echo $mylocalhost;?>/assets/img/side5.png" />การเงิน</a><img  alt="" />
+        <li><a href="#" class="sub" tabindex="1"><img src="<?php echo $vps;?>/assets/img/side5.png" />การเงิน</a><img  alt="" />
             <ul>
                 <li><a href="#">รายรับของฉัน</a></li>
                 <li><a href="#">Seller Balance</a></li>
@@ -71,25 +71,25 @@
                 <li><a href="#">ตั้งค่าการชำระเงิน</a></li>
             </ul>
         </li>
-        <li><a href="#" class="sub" tabindex="1"><img src="<?php echo $mylocalhost;?>/assets/img/side6.png" />ข้อมูล</a><img  alt="" />
+        <li><a href="#" class="sub" tabindex="1"><img src="<?php echo $vps;?>/assets/img/side6.png" />ข้อมูล</a><img  alt="" />
             <ul>
                 <li><a href="#">Business Insights</a></li>
                 <li><a href="#">Account Health</a></li>
             </ul>
         </li>
-        <li><a href="#" class="sub" tabindex="1"><img src="<?php echo $mylocalhost;?>/assets/img/side7.png" />การเติบโต</a><img  alt="" />
+        <li><a href="#" class="sub" tabindex="1"><img src="<?php echo $vps;?>/assets/img/side7.png" />การเติบโต</a><img  alt="" />
             <ul>
                 <li><a href="#">Seller Missions</a></li>
                 <li><a href="#">ร้านค้าแนะนำ</a></li>
             </ul>
         </li>
-        <li><a href="#" class="sub" tabindex="1"><img src="<?php echo $mylocalhost;?>/assets/img/side8.png" />การบริการลูกค้า</a><img  alt="" />
+        <li><a href="#" class="sub" tabindex="1"><img src="<?php echo $vps;?>/assets/img/side8.png" />การบริการลูกค้า</a><img  alt="" />
             <ul>
                 <li><a href="#">ตัวช่วยตอบแชท</a></li>
                 <li><a href="#">ตัวช่วยจัดการ FAQ</a></li>
             </ul>
         </li>
-        <li><a href="#" class="sub" tabindex="1"><img src="<?php echo $mylocalhost;?>/assets/img/side9.png" />ร้านค้า</a><img  alt="" />
+        <li><a href="#" class="sub" tabindex="1"><img src="<?php echo $vps;?>/assets/img/side9.png" />ร้านค้า</a><img  alt="" />
             <ul>
                 <li><a href="#">คะแนนร้านค้า</a></li>
                 <li><a href="#">รายละเอียดร้านค้า</a></li>
@@ -98,7 +98,7 @@
                 <li><a href="#">รายงานของฉัน</a></li>
             </ul>
         </li>
-        <li><a href="#" class="sub" tabindex="1"><img src="<?php echo $mylocalhost;?>/assets/img/side10.png" />การตั้งค่า</a><img  alt="" />
+        <li><a href="#" class="sub" tabindex="1"><img src="<?php echo $vps;?>/assets/img/side10.png" />การตั้งค่า</a><img  alt="" />
             <ul>
                 <li><a href="#">ที่อยู่ของฉัน</a></li>
                 <li><a href="#">ตั้งค่าร้านค้า</a></li>
